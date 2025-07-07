d4d43fd4bfb75d881a46208e41fed024b5daf0e9
ayyap <ayyap@venkyvenkat>
2025-07-07 07:45:46 +0530

    initial commit

---
+++ eclipse-workspace/secondprogram/src/secondprogram/practice.java
@@ -0,0 +1,15 @@
+package secondprogram;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 27;
+		int c = a+b;
+		System.out.println("the addition of a+b is c= "+ c);
+
+	}
+
+}
