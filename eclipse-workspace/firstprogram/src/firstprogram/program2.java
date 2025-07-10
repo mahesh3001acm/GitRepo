557f3993e8fada904bfeb3555edb786c5fab3c69
ayyap <ayyap@mahesh>
2025-07-10 10:34:13 +0530

    new branch creation

---
+++ eclipse-workspace/firstprogram/src/firstprogram/program2.java
@@ -0,0 +1,10 @@
+package firstprogram;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("HELLO JAVA");
+	}
+
+}
